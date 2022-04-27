2292c17a14793db6b100a6fefcf865eda4d74a23
GmoWilliams <guillermowilliams01@gmail.com>
2022-04-27 15:55:25 -0700

    Excel files to MongoDB functioning

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const multer = require("multer");
+const XLSX = require('xlsx');
 const cors = require("cors");
 
 const app = express();
@@ -12,17 +14,31 @@ const key = 'inshotmongodb';
 // conectar atlas
 mongoose.connect(`mongodb+srv://DBAdmin:${key}@clustermiapp.jwlpc.mongodb.net/EafyAppDB`, {useNewUrlParser: true});
 
+// multer
+var storage = multer.memoryStorage();
+
+var upload = multer({ storage: storage });
+
 //Base de datos
 //1. Esquema
 //const Schema = mongoose.Schema;
 const usuarioSchema = {
     nombre: String,
     email: String,
-    contraseña: String
+    contraseña: String,
+    reportesGenerados: Number
 };
+
+const excelSchema = new mongoose.Schema ({
+    Item: Number,
+    Description: String,
+    UM: String,
+    ProductCode: String
+});
 
 // 2. crear el modelo
 const Usuario = new mongoose.model("Usuario", usuarioSchema);
+var excelModel = new mongoose.model("Excel", excelSchema);
 
 //Método post
 app.post("/registrar", function (req, res){
@@ -35,11 +51,30 @@ app.post("/registrar", function (req, res){
     const usuarioBaseDatos = new Usuario ({
         nombre: usuarioFormulario,
         email: emailFormulario,
-        contraseña: contraseñaFormulario
+        contraseña: contraseñaFormulario,
+        reportesGenerados: 0
     });
 
     //4. subir a la base de datos o guardar.
     usuarioBaseDatos.save();
+});
+
+app.post("/subirExcel", upload.single('excel'), (req, res) => {
+    var workbook = XLSX.read(req.file.buffer);
+    var sheet_namelist = workbook.SheetNames;
+    var x=0;
+    sheet_namelist.forEach(element => {
+        var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_namelist[x]]);
+        excelModel.insertMany(xlData, (err,data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(data);
+            }
+        })
+        x++;
+    });
+    res.redirect('/');
 });
 
 //////// 2 fragmentos necesarios para implementar heroku
